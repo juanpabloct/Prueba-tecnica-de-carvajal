1c1b6595340c3f47bdf04e9a0187ae92ead76c1e
Juan Pablo Castellanos Torres <juanptorres2003@gmail.com>
2022-03-04 12:15:07 -0500

    Se ha creado los archivos necesarios para el backend y las rutas en las que se va a realizar crud

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const port = 9000;
+const morgan = require("morgan");
+
+const taskRoutes = require("./routes/task.routes.js");
+
+app.use(morgan("dev"));
+//Routes
+app.use(taskRoutes);
+
+//Arranque
+app.listen(port);
+console.log(`The server is in port ${port}`);
